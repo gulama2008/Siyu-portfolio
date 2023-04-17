23f71f82bf6870b3e083fe7668975b30304079d6
Siyu Liu <siyu.java.web@gmail.com>
2023-04-17 23:15:19 +1000

    make navbar item highlight when on the page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,10 @@ export default function Navigation({ currentPage, handlePageChange }) {
     <ul className="navbar">
       <li className="navbar-list-item">
         <a
-          className="navbar-link"
-          href="#home"
+          className={
+            currentPage === "AboutMe" ? "navbar-link-active" : "navbar-link"
+          }
+          href="#AboutMe"
           onClick={() => handlePageChange("AboutMe")}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -20,8 +22,10 @@ export default function Navigation({ currentPage, handlePageChange }) {
       </li>
       <li className="navbar-list-item">
         <a
-          className="navbar-link"
-          href="#about"
+          className={
+            currentPage === "Portfolio" ? "navbar-link-active" : "navbar-link"
+          }
+          href="#Portfolio"
           onClick={() => handlePageChange("Portfolio")}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           // className={
@@ -33,8 +37,10 @@ export default function Navigation({ currentPage, handlePageChange }) {
       </li>
       <li className="navbar-list-item">
         <a
-          className="navbar-link"
-          href="#blog"
+          className={
+            currentPage === "Contact" ? "navbar-link-active" : "navbar-link"
+          }
+          href="#Contact"
           onClick={() => handlePageChange("Contact")}
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           // className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
@@ -44,8 +50,10 @@ export default function Navigation({ currentPage, handlePageChange }) {
       </li>
       <li className="navbar-list-item">
         <a
-          className="navbar-link"
-          href="#contact"
+          className={
+            currentPage === "Resume" ? "navbar-link-active" : "navbar-link"
+          }
+          href="#Resume"
           onClick={() => handlePageChange("Resume")}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           // className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
